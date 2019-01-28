414b0fdd15168973dfe99e21641e677dba320c1c
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-01-28 12:37:04 +0700

    add cron in main.go and edited typo in handlers User

--- handlers/user.go
+++ handlers/user.go
@@ -15,7 +15,7 @@ func User() {
 	for i, u := range users {
 		fmt.Sprintf("Processing User #%d", i)
 		if err := transformer.SQLUser(u); err != nil {
-			panic
+			panic(err)
 		}
 	}
 	fmt.Printf("Done! %d data processed!\n")
--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"time"
 
 	"kurapika/handlers"
+	"github.com/robfig/cron"
 )
 
 func main() {
 
-	fmt.Println("Hello World!")
+	fmt.Println("Kurapika loading..")
+	c := cron.New()
+
+	c.AddFunc("0 10 15 * * 6", extract)
+	go c.Start()
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+	<-sig
 
 }
 
