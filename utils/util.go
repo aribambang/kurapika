e53a9ac3999ae9ce4e4883e951d23d1a90c3947c
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-01-28 11:23:25 +0700

    add func DateDim in util

--- utils/util.go
+++ utils/util.go
@@ -4,6 +4,14 @@ import (
 	"kurapika/config"
 )
 
+type date struct {
+	Day        int
+	Week       int
+	Month      int
+	Year       string
+	ActionDate string
+}
+
 func ExecQuery(q string, p []interface{}) error {
 	db := config.MysqlDB()
 
@@ -22,4 +30,35 @@ func ExecQuery(q string, p []interface{}) error {
 
 	return nil
 
+}
+
+func DateDim(dc date) (dateID int, err error) {
+	db := config.MysqlDB()
+
+	stmt, err := db.Prepare("CALL dateDimension(?, ?, ?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(dc.Day, dc.Week, dc.Month, dc.Year, dc.ActionDate)
+	if err != nil {
+		panic(err)
+	}
+
+	row, err := db.Query("SELECT @dateId")
+	if err != nil {
+		panic(err)
+	}
+
+	defer row.Close()
+
+	for row.Next() {
+		row.Scan(&dateID)
+	}
+
+	defer db.Close()
+
+	return dateID, nil
 }
