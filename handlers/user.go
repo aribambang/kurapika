218b48e638210aadf6a5ecaa4d3dea365962ef86
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-01-28 12:12:37 +0700

    add file handlers User

---
+++ handlers/user.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"fmt"
+
+	"kurapika/extractor"
+	"kurapika/transformer"
+)
+
+func User() {
+	users, err := extractor.MgoUser()
+	if err != nil {
+		panic(err)
+	}
+	for i, u := range users {
+		fmt.Sprintf("Processing User #%d", i)
+		if err := transformer.SQLUser(u); err != nil {
+			panic
+		}
+	}
+	fmt.Printf("Done! %d data processed!\n")
+}
